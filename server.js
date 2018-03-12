e160bd42575bf12a619e10851224efa26c2ea862
Volkova Annie <annie_v@bk.ru>
2018-03-13 01:20:38 +0300

    WIP server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const express = require('express');
-const { createBundleRenderer } = require('vue-server-renderer');
+const fs = require('fs')
+const express = require('express')
+const { createBundleRenderer } = require('vue-server-renderer')
 
 const bundleRenderer = createBundleRenderer(
   // Load the SSR bundle with require.
@@ -11,13 +11,13 @@ const bundleRenderer = createBundleRenderer(
     // Yes, I know, readFileSync is bad practice. It's just shorter to read here.
     template: fs.readFileSync('./index.html', 'utf-8')
   }
-);
+)
 
 // Create the express app.
-const app = express();
+const app = express()
 
 // Serve static assets from ./dist on the /dist route.
-app.use('/dist', express.static('dist'));
+app.use('/dist', express.static('dist'))
 
 // Render all other routes with the bundleRenderer.
 app.get('*', (req, res) => {
@@ -25,8 +25,8 @@ app.get('*', (req, res) => {
     // Renders directly to the response stream.
     // The argument is passed as "context" to main.server.js in the SSR bundle.
     .renderToStream({url: req.path})
-    .pipe(res);
-});
+    .pipe(res)
+})
 
 // Bind the app to this port.
-app.listen(8080);
+app.listen(8080)
